3f2d8309486f7e6d4904e9af0e5b72b2c6dff42a
nkrishang <kn1457@nyu.edu>
2021-07-29 11:01:02 -0400

    Landing complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 import { Button } from '@chakra-ui/button'
-import { ArrowForwardIcon } from '@chakra-ui/icons' 
+import { ArrowForwardIcon, ArrowDownIcon } from '@chakra-ui/icons' 
 
 export default function Home() {
   return (
@@ -13,48 +13,74 @@ export default function Home() {
         <link rel="icon" href="/niftyswap-logo.ico" />
       </Head>
       
-      <div className="border border-red-600 max-w-6xl m-auto">
-        <div className="flex flex-col border border-black">
+      <div className="max-w-6xl m-auto px-8 py-8">
+        <div className="flex flex-col">
 
-          <div className="flex flex-row justify-between py-8">
+          <div className="flex flex-row justify-between pb-8">
             <Image
-              src="/niftyswap-logo-full.png"
+              src="/niftyswap-logo-full.svg"
               height={80}
               width={239}
             />
 
-            <div className="flex flex-row justify-center border border-black items-center">
-              <a className="mr-4">
-                source code
+            <div className="flex flex-row justify-center items-center">
+              <a className="mr-4 hover:underline cursor-pointer">
+                smart contract
               </a>
 
-              <a className="ml-4">
+              <a className="ml-4 hover:underline cursor-pointer">
                 github
               </a>
             </div>
           </div>
         </div>
 
-        <div className="flex flex-row m-auto">
-          <div className="flex flex-col justify-center w-2/3">
-            <p className="text-gray-800 text-6xl font-black leading-relaxed py-4">
-              Trade NFTs with your (<span className="text-purple-400 font-black text-6xl">internet</span>) friends.
-            </p>
+        <div className="flex flex-row m-auto justify-between">
+          <div className="flex flex-col justify-start w-2/3">
+						<p className="text-gray-800 text-7xl font-black leading-normal py-4">
+							Trade NFTs with your (<span className="text-purple-400 font-black text-6xl">internet</span>) friends.
+						</p>
 
-            <p className="text-gray-600 text-xl pt-4 pb-8 font-semibold">
-              Share a ‘swap link’ to your NFT and find people who’ll trade their NFTs for yours!
-            </p>
+						<p className="text-gray-600 text-xl pt-4 pb-8">
+							Share a ‘swap link’ to your NFT and find people who’ll trade their NFTs for yours!
+						</p>
 
-            <Button rightIcon={<ArrowForwardIcon />} variant="solid" colorScheme="purple" maxW="250">
-              Enter App
-            </Button>
+						<Button rightIcon={<ArrowForwardIcon />} variant="solid" colorScheme="purple" maxW="250" size="lg">
+							Enter App
+						</Button>
           </div>
 
-          <Image
-            height={400}
-            width={160}
-            src="/niftyswap-landing-graphic.png"
-          />
+					<div className="flex flex-col justify-center items-center" style={{maxHeight: "560px" }}>
+						<div className="h-60 w-60 border-2 border-pink-500 bg-white shadow-neonpink rounded-lg flex flex-col justify-center z-10">
+							<div className="m-auto">
+								<Image
+									priority={true}
+									src="/niftyswap-landing-cool-cats.png"
+									height={200}
+									width={200}
+								/>
+							</div>
+						</div>
+
+						<div className="h-16 w-4 bg-black z-0 m-auto">
+							<div className="bg-white border-2 border-purple-700 h-10 w-10 rounded-xl z-10 m-auto relative right-3 top-3 flex flex-col justify-center">
+								<div className="m-auto">
+									<ArrowDownIcon w={6} h={6} color="purple.500"/>
+								</div>
+							</div>
+						</div>						
+
+						<div className="h-60 w-60 border-2 border-blue-500 bg-white shadow-neonblue rounded-lg flex flex-col justify-center z-10">
+							<div className="m-auto">
+								<Image
+									priority={true}
+									src="/niftyswap-landing-bored-ape.png"
+									height={200}
+									width={200}
+								/>
+							</div>
+						</div>
+					</div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    boxShadow: {
+      neonpink: "0 0 50px rgba(205, 13, 163, 0.25)",
+      neonblue: "0 0 50px rgba(105, 177, 244, 0.25)"
+    },
     extend: {},
   },
   variants: {
